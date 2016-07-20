340523dd360a3a3b6c62cf6dce028bdb466d1702
Vitkyk <pashcin2000@mail.ru>
2016-07-20 20:09:02 +0300

    Changed url field to id field

--- lobby/serializers.py
+++ lobby/serializers.py
@@ -6,10 +6,10 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('id', 'username', 'email', 'groups')
 
 
 class MessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Message
-        fields = ('url', 'sender', 'receiver', 'text')
+        fields = ('id', 'sender', 'receiver', 'text')
